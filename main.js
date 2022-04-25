ee5b7587836d460a546966ac7a241a85216bbdd1
Allwell Onen <aleenfestus@gmail.com>
2022-04-25 23:11:33 +0100

    Implement file writing and opening functionality
    Signed-off-by: Allwell Onen <aleenfestus@gmail.com>

--- main.js
+++ main.js
@@ -1,19 +1,134 @@
-const { BrowserWindow, app } = require("electron");
+const {
+  BrowserWindow,
+  app,
+  ipcMain,
+  dialog,
+  Notification,
+  Menu,
+} = require("electron");
 require("electron-reloader")(module);
 const path = require("path");
+const fs = require("fs");
+
+const ext = [
+  "txt",
+  "py",
+  "java",
+  "cpp",
+  "c",
+  "kt",
+  "js",
+  "jsx",
+  "ts",
+  "tsx",
+  "md",
+  "html",
+  "css",
+  "json",
+  "php",
+];
+
+let mainWindow;
+let openedFilePath;
+const handleError = () => {
+  new Notification({
+    title: "Error",
+    body: "Something went wrong :(",
+  });
+};
 
 const createWindow = () => {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    frame: false,
+    // frame: false,
     titleBarStyle: "hiddenInset",
     webPreferences: {
       preload: path.join(app.getAppPath(), "renderer.js"),
     },
   });
 
-  // mainWindow.webContents.openDevTools();
   mainWindow.loadFile("index.html");
+
+  const menuTemplate = [
+    {
+      label: "File",
+      submenu: [
+        {
+          label: "Open File",
+          click: () => ipcMain.emit("open-document-triggered"),
+        },
+        {
+          label: "Create New File",
+          click: () => ipcMain.emit("create-document-triggered"),
+        },
+        {
+          role: "quit",
+        },
+      ],
+    },
+  ];
+
+  const menu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(menu);
 };
+
 app.whenReady().then(createWindow);
+
+ipcMain.on("open-document-triggered", () => {
+  dialog
+    .showOpenDialog({
+      properties: ["openFile"],
+      filters: [
+        {
+          name: "Files",
+          extensions: ext,
+        },
+      ],
+    })
+    .then(({ filePaths }) => {
+      const filePath = filePaths[0];
+
+      fs.readFile(filePath, "utf8", (error, content) => {
+        if (error) {
+          // console.log("open-document-triggered", error);
+          handleError();
+        } else {
+          openedFilePath = filePath;
+          mainWindow.webContents.send("document-opened", { filePath, content });
+        }
+      });
+    });
+});
+
+ipcMain.on("create-document-triggered", () => {
+  dialog
+    .showSaveDialog(mainWindow, {
+      filters: [
+        {
+          name: "Files",
+          extensions: ext,
+        },
+      ],
+    })
+    .then(({ filePath }) => {
+      fs.writeFile(filePath, "", (error) => {
+        if (error) {
+          // console.log("create-document-triggered", error);
+          handleError();
+        } else {
+          openedFilePath = filePath;
+          mainWindow.webContents.send("document-created", filePath);
+        }
+      });
+    });
+});
+
+ipcMain.on("file-updated", (_, editorContent) => {
+  fs.writeFile(openedFilePath, editorContent, (error) => {
+    if (error) {
+      // console.log("file-updated", error);
+      handleError();
+    }
+  });
+});
